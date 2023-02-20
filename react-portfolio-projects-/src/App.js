1cf63981b1bb42d6aaf1dfdde4468ed9abf0feef
Americano.Dev <andrenations@gmail.com>
2023-02-19 23:54:01 -0500

    fixed componets & page name

--- react-portfolio-projects-/src/App.js
+++ react-portfolio-projects-/src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import About from  './components/About';
-import Project from  './components/Project';
+import About from './pages/About';
+import Project from './pages/Project';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navbar />} />
         <Route path="/" element={<About />} />
         <Route path="/" element={<Project />} />
-  
-        
+        <Route path="/" element={<Footer />} />
+
+
+
       </Routes>
 
     </Router>
