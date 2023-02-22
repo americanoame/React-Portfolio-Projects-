d7df739ea9c7b2d6efe5709c57d34277b2c96d16
Americano.Dev <andrenations@gmail.com>
2023-02-22 05:43:59 -0500

    added text on the about page

--- react-portfolio-projects-/src/components/About.js
+++ react-portfolio-projects-/src/components/About.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import photo1 from "../images/photo1.jpeg";
+import '../About.css';
 
 export default function About() {
     return (
-
-        <section id='about' className='py-5'>
+        <section className='py-5'>
 
             <div className='container'>
-                <div class='row'>
-                    <div class='col-md-5 mx-auto text-center'>
+                <div className='row'>
+                    <div className='col-lg-6 mx-auto text-center'>
                         <img src={photo1} alt='andre' className='img-fluid mb-5 rounded-circle' />
                         <h2 className='text-secondary'>Andre Silva</h2>
-                        <p className='lead text-muted mb-5'>a paxon developer</p>
+                        <p className='lead text-muted mb-5'>Coding life | Golfing Life</p>
                     </div>
                 </div>
 
+                <div  id='background-color' className='row'>
+                    <div className='col-lg-6 mx-auto'>
 
-                
+                        <p className='lead text-muted mb-3'>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        <p className='lead text-muted mb-3'>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        <p className='lead text-muted mb-3'>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        <p className='lead text-muted mb-3'>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        <p className='lead text-muted mb-3'>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        <p className='lead text-muted mb-3'>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+
+                    </div>
+                </div>
+
             </div>
 
         </section>
