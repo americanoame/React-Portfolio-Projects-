c5f6e90023e41c5b5580765341a1e6a074cb7256
Americano.Dev <andrenations@gmail.com>
2023-02-21 20:13:03 -0500

    changed main photo

--- react-portfolio-projects-/src/components/About.js
+++ react-portfolio-projects-/src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import photo from "../images/photo.jpg";
+import photo1 from "../images/photo1.jpeg";
 
 export default function About() {
     return (
@@ -9,7 +9,7 @@ export default function About() {
             <div className='container'>
                 <div class='row'>
                     <div class='col-md-5 mx-auto text-center'>
-                        <img src={photo} alt='andre' className='img-fluid mb-5 rounded-circle' />
+                        <img src={photo1} alt='andre' className='img-fluid mb-5 rounded-circle' />
                         <h2 className='text-secondary'>Andre Silva</h2>
                         <p className='lead text-muted mb-5'>a paxon developer</p>
                     </div>
