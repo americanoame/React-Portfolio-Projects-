83bb05b04db202f47a74687df6a9730d818b8647
Americano.Dev <andrenations@gmail.com>
2023-02-21 19:38:22 -0500

    added the final footer style

--- react-portfolio-projects-/src/components/Footer/index.js
+++ react-portfolio-projects-/src/components/Footer/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import './Footer.css';
 
-
 const Footer = () => (
-  <footer>
+  <footer className='navbar d-flex justify-content-evenly'>
     <ul className="footer">          
-          <li ><a href="" target="_blank"><FaLinkedinIn style={{ fontSize: '50px' }}/></a></li>
+          <li ><a href="" target="_blank"><FaLinkedin style={{ fontSize: '50px' }}/></a></li>
           <li ><a href="" target="_blank"><FaGithub style={{ fontSize: '50px' }}/></a></li>                
         </ul>
     <p className="text-center">Copyright © {new Date().getFullYear()} Andre Silva</p>
