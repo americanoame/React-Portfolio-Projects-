68f0c0ee62df4ac3221f086a665082c8239f1e45
Americano.Dev <andrenations@gmail.com>
2023-02-25 04:45:47 -0500

    adding styles & fixing the images

--- react-portfolio-projects-/src/components/Portfolio.js
+++ react-portfolio-projects-/src/components/Portfolio.js
@@ -1,15 +1,15 @@
 import Projects from './Projects';
-import img1 from '../images/Good1.png';
-import img2 from '../images/Good2.png';
-import img3 from '../images/Good1.png';
-import img4 from '../images/Good2.png';
-import img5 from '../images/Good1.png';
+// import img1 from '../images/Good1.png';
+import img2 from '../images/E-commerce.png';
+// import img3 from '../images/Good1.png';
+// import img4 from '../images/Good2.png';
+// import img5 from '../images/Good1.png';
 
 export default function Portfolio() {
 
   const images = [
     {
-      src: img1,
+      // src: img1,
       alt: 'Note-Taker',
       title: 'Redux-e-commerce-platform',
       description: 'An Express.js App',
@@ -17,24 +17,25 @@ export default function Portfolio() {
     {
       src: img2,
       alt: 'Redux',
-      title: 'Redux-e-commerce-platform',
-      description: 'Redux',
-      link: 'https://github.com/americanoame/Redux-e-commerce-platform'
+      title: 'GitHub Repo',
+      description: 'Deployed',
+      link: 'https://github.com/americanoame/Redux-e-commerce-platform',
+      deployed: 'https://redux-e-commerce.herokuapp.com'
     },
     {
-      src: img3,
+      // src: img3,
       alt: 'Note-Taker',
       title: 'Note-Taker',
       description: 'An Express.js App',
     },
     {
-      src: img4,
+      // src: img4,
       alt: 'Note-Taker',
       title: 'Note-Taker',
       description: 'An Express.js App',
     },
     {
-      src: img5,
+      // src: img5,
       alt: 'Note-Taker',
       title: 'Note-Taker',
       description: 'An Express.js App',
--- react-portfolio-projects-/src/components/Projects.js
+++ react-portfolio-projects-/src/components/Projects.js
@@ -6,8 +6,8 @@ export default function Projects({ images }) {
     console.log(images)
     return (
         <>
-            {images.map(({ src, alt, title, description, link}) => (
-                <div id='backgoun2' className="col-12 col-sm-6 col-lg-4" key={alt}>
+            {images.map(({ src, alt, title, description, link, deployed}) => (
+                <div id='background2' className="col-12 col-sm-6 col-lg-4" key={alt}>
 
                     <div className="container">
                         <img src={src} className="card-img-top" alt={alt} />
@@ -15,7 +15,8 @@ export default function Projects({ images }) {
                     </div>
                     <div className="card-body">
                         <a href={link} className="card-title">{title}</a>
-                        <p className="card-text pb-3">{description}</p>
+                        <a href={deployed} className="card-description">{description}</a>
+                        <p className="card-text pb-3">{alt}</p>
                     </div>
                 </div>
             ))}
