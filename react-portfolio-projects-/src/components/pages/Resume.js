290e062c0d6525b3a2c679270dda6f389a13a2ae
Americano.Dev <andrenations@gmail.com>
2023-02-26 23:17:34 -0500

    refactoring resume page, change the text & fix the background collor of the website

--- react-portfolio-projects-/src/components/pages/Resume.js
+++ react-portfolio-projects-/src/components/pages/Resume.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import Cv1 from "../../images/Cv1.jpg";
+import '../../styles/Resume.css';
 
 export default function Resume() {
 
-
-
-
     return (
 
-        <div className="container my-5">
+        <div className="container-resume my-5">
 
             <div className='d-flex justify-content-end'>
                 <button onClick={Resume} className="btn btn-lg btn-primary download-button">Download PDF</button>
             </div>
 
             <div className="row">
-                <div class="col-lg-4">
+                <div className="col-lg-2">
                     <img src={Cv1} alt='andre' className='img-fluid mb-5 rounded-circle' />
-                    <h2 className="mb-4">John Doe</h2>
-                    <p className="mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac est eu est aliquam dictum. Integer tristique mauris eu malesuada rutrum. Aenean luctus malesuada neque, eu posuere felis bibendum non. Suspendisse nec risus sit amet lacus fringilla elementum. Donec vestibulum massa vel lacus vehicula faucibus.</p>
-                    <h3 className="mb-3">Skills</h3>
-                    <ul className="list-unstyled">
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>Bootstrap</li>
-                    </ul>
+                    <hr className="header-hr" />
+                    
                 </div>
+
                 <div className="col-lg-8">
-                    <h2 className="mb-4">Education</h2>
-                    <div className="card mb-4">
-                        <div className="card-body">
-                            <h3 className="card-title">Bachelor of Science, Computer Science</h3>
-                            <h4 className="card-subtitle mb-2 text-muted">University of California, Los Angeles</h4>
-                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac est eu est aliquam dictum. Integer tristique mauris eu malesuada rutrum.</p>
-                        </div>
-                    </div>
-                    <div className="card mb-4">
+                    <h1 className="headerName">Andre Silva</h1>
+                    <h3 className="headerTask">
+                        Full Stack Web Developer / Caddie / Golf Player
+                    </h3>
+                    <hr className="header-hr" />
+                    <h3 className="headerSummary">
+    
+                        am a highly motivated and web developer recent grauduated from Rutgers Coding Bootcamp.
+
+                        Throughout my studies, I have consistently demonstrated my passion
+
+                        for creating innovative and user-friendly websites that meet the needs of diverse audiences. I have had the opportunity
+
+                    </h3>
+
+
+
+                    <div className="card mt-5">
                         <div className="card-body">
                             <h3 className="card-title">Master of Science, Computer Science</h3>
                             <h4 className="card-subtitle mb-2 text-muted">Stanford University</h4>
@@ -45,9 +45,12 @@ export default function Resume() {
                         </div>
                     </div>
 
+
+
                 </div>
 
             </div>
+
 
         </div>
 
