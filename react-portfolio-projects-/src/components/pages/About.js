a25a2174a6dd303215d6e0a9b8ad39845232a6ba
Americano.Dev <andrenations@gmail.com>
2023-03-01 05:34:55 -0500

    fixed the about text & styles flags

--- react-portfolio-projects-/src/components/pages/About.js
+++ react-portfolio-projects-/src/components/pages/About.js
@@ -11,18 +11,28 @@ export default function About() {
                     <div className='col-lg-6 mx-auto text-center'>
                         <img src={photo1} alt='andre and Saul' className='img-fluid mb-5 rounded-circle' />
                         <h2 className='--Full-Stack-text'>Full-Stack Web Developer</h2>
-                        <p className='--text-marketing'>Crafting experiences & Creating digital solutions to make the world a better place.</p>
+                        <p className='--text-marketing'>Crafting experiences & Creating digital
+                            solutions to make the world a better place.
+                        </p>
                     </div>
                 </div>
-
                 <div id='--section-about-me-text' className='row'>
                     <div className='col-lg-6 mx-auto'>
-                        <p className='-about-me-text'>Hello hello🇧🇷  I'm andre, a web developer recent grauduated from Rutgers Coding Bootcamp.</p>
-                        <p className='-about-me-text'>and i am so happy to be able to share my journey in this little peace of art.</p>
-                        <p className='-about-me-text'>Throughout my studies, I have consistently demonstrated my passion </p>
-                        <p className='-about-me-text'>for creating innovative and user-friendly websites that meet the needs of diverse audiences. I have had the opportunity </p>
-                        <p className='-about-me-text'>to work on a variety of projects and technologies, which has allowed me to develop a well-rounded skill set </p>
-                        <p className='-about-me-text'>and deep understanding of web development. I am always eager to expand my knowledge and take on new challenges. </p>
+                        <p className='-about-me-text'>Hello hello<span className='my-flags'>🇧🇷🇺🇸</span>  I'm andre, a web developer recent
+                            graduated from Rutgers Coding Bootcamp. and i am so happy to be able to share
+                            my journey in this little peace of art.
+                        </p>
+                        <p className='-about-me-text'>Throughout my studies, I have consistently demonstrated
+                            my passion for creating innovative and user-friendly websites that meet the needs of
+                            diverse audiences.
+                        </p>
+                        <p className='-about-me-text'> I have had the opportunity to work on a variety of
+                            projects and technologies, which has allowed me to develop a well-rounded skill set
+                        </p>
+
+                        <p className='-about-me-text'>and deep understanding of web development. I am always
+                            eager to expand my knowledge and take on new challenges.
+                        </p>
                     </div>
                 </div>
             </div>
